273a5bc40f9ee14a122cba89fb62c63ad3399b71
Tenacious-Qi <joe.mccann@mailbox.org>
2020-02-02 22:38:54 -0500

    divs change color on mouseenter

--- main.js
+++ main.js
@@ -2,7 +2,13 @@ const container = document.querySelector('#container');
 
 for (let i = 0; i < 256; i++) {
     const gridDiv = document.createElement('div');
-    gridDiv.classList.add('box');
     container.appendChild(gridDiv);
+    gridDiv.classList.add('box');
 }
 
+let boxes = Array.from(document.querySelectorAll('.box'));
+for (let i = 0; i < boxes.length; i++) {
+    boxes[i].addEventListener('mouseenter', function() {
+        boxes[i].classList.add('active');
+    });
+}
