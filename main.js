506023d517c1ee5126a8b74efaab2d576e96816d
Tenacious-Qi <joe.mccann@mailbox.org>
2020-02-04 21:33:54 -0500

    successfully coded 'new grid' functionality using css grid

--- main.js
+++ main.js
@@ -9,20 +9,32 @@ function makeGrid(gridSize) {
 }
 
 makeGrid(32);
-
-let boxes = Array.from(document.querySelectorAll('.box'));
-for (let i = 0; i < boxes.length; i++) {
-    boxes[i].addEventListener('mouseenter', function() {
+function activateBoxes() {
+    const boxes = Array.from(document.querySelectorAll('.box'));
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].addEventListener('mouseenter', function() {
         boxes[i].classList.add('active');
-    });
+        });
+    }
 }
+activateBoxes();
 
 const button = document.querySelector('.btn-clear');
-button.addEventListener('click', function() {
-    for (let i = 0; i < boxes.length; i++) {
-        boxes[i].classList.remove('active');
+button.addEventListener('click', newGrid);
+
+function newGrid() {
+    let userInput = prompt('how many boxes per side? Recommended: 12 - 128', 32);
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild);
+      }
+      makeGrid(userInput);
+      activateBoxes();
+
+      const boxes = Array.from(document.querySelectorAll('.box'));
+      for (let i = 0; i < boxes.length; i++) {
+          boxes[i].style.width = 960 / Number(userInput)  + 'px'; // container width divided by grid width
+          boxes[i].style.height = 640 / Number(userInput) + 'px';
     }
-});
-
-
-
+    container.style.gridTemplateColumns = 'repeat(' + Number(userInput) + ', 1fr)';
+    container.style.gridTemplateRows = 'repeat(' + Number(userInput) + ', 1fr)';
+}
