ada1dcc4b91b16d6b0245f200eec21403c56627a
Tenacious-Qi <joe.mccann@mailbox.org>
2020-02-05 19:43:05 -0500

    added a range input for grid size selection, rearranged javascript, updated css

--- main.js
+++ main.js
@@ -19,21 +19,32 @@ function activateBoxes() {
 }
 activateBoxes();
 
-const button = document.querySelector('.btn-clear');
-button.addEventListener('click', newGrid);
-
-function newGrid() {
-    let userInput = prompt('how many boxes per side? Recommended: 12 - 128', 32);
+const changeSize = document.querySelector('#change-size');
+const inputValue = document.querySelector('.input-value');
+function updateGridSize() {
     while (container.hasChildNodes()) {
         container.removeChild(container.lastChild);
       }
-      makeGrid(userInput);
+      makeGrid(this.value);
       activateBoxes();
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].style.width = 768 / this.value + 'px'; // container width divided by grid width
+        boxes[i].style.height = 480 / this.value + 'px';
+  }
+    container.style.setProperty('grid-template-columns', 'repeat(' + this.value + ', 1fr)');
+    container.style.setProperty('grid-template-rows', 'repeat(' + this.value + ', 1fr)');
+    inputValue.textContent = `${this.value}`
+}
+changeSize.addEventListener('change', updateGridSize);
+changeSize.addEventListener('mousemove', function() {
+    inputValue.textContent = `${this.value}`
+});
 
-      for (let i = 0; i < boxes.length; i++) {
-          boxes[i].style.width = 768 / Number(userInput)  + 'px'; // container width divided by grid width
-          boxes[i].style.height = 480 / Number(userInput) + 'px';
+const buttonClear = document.querySelector('.btn-clear');
+buttonClear.addEventListener('click', clear);
+
+function clear() {
+    for (let i = 0; i < boxes.length; i++) {
+        boxes[i].classList.remove('active')
     }
-    container.style.gridTemplateColumns = 'repeat(' + Number(userInput) + ', 1fr)';
-    container.style.gridTemplateRows = 'repeat(' + Number(userInput) + ', 1fr)';
 }
